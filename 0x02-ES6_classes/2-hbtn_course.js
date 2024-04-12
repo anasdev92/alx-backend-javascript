7449ab0b8834e186b0c9f5b2442cdcdddc938e18
anasdev92 <elyamni.anas@gmail.com>
2024-04-12 06:27:38 -0700

    0x02-ES6_classes

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonCourse {
+  /**
+   * Creates a new HolbertonCourse class
+   *
+   * @param {String} name - The name of the course.
+   * @param {Number} length - How long the course is (in months).
+   * @param {String[]} students - The names of students in the course.
+   *
+   * Implement getters and setters for each attribute
+   */
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
